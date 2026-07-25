36849a92047aa52c08ae003c785547462fb8d28c
Reza Luna 2501091005 <rezaluna2313@gmail.com>
2026-07-25 15:41:25 +0700

    Update FormMahasiswa.java

--- FormMahasiswa.java
+++ FormMahasiswa.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ervan180626;
+package rza180626;
 
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
